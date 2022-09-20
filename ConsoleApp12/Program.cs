450eec6d0db8a1739fd04a0f31ef101abb0e9520
Rahuldal123 <rahuldalwale55@gmail.com>
2022-09-20 08:10:34 +0530

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public interface Iorder
+    {
+        string DisplayDetails();
+    }
+    public interface Icustomer
+    {
+        string DisplayDetails();
+    }
+    public class Transaction : Iorder, Icustomer
+    {
+        string Iorder.DisplayDetails()
+        {
+            return $"order details";
+        }
+
+        string Icustomer.DisplayDetails()
+        {
+            return $"customer details";
+        }
+    
+
+
+
+        static void Main(string[] args)
+           {
+
+            Iorder o1 = new Transaction();
+            
+            Console.WriteLine(o1.DisplayDetails());
+
+            Icustomer c1 = new Transaction();
+            Console.WriteLine(c1.DisplayDetails());
+
+          
+        }
+    }
+}
